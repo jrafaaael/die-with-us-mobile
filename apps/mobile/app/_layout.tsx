ebb7b41d89063c3eae8f452096d28c3c88026358
jrafaaael <jrmd070102@gmail.com>
2023-04-27 17:14:16 -0400

    feat(mobile): wrap app in `_layout`

---
+++ apps/mobile/app/_layout.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { COLORS, SPACING } from "../src/utils/styles";
+
+export default function MainLayout() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <SafeAreaView style={styles.container}>
+        <Slot />
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: SPACING.base,
+    backgroundColor: COLORS.stone[950],
+    flex: 1,
+  },
+});
