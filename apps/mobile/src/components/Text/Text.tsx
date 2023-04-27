765d815a4e261f8fcf139f5eb5f1761636f6732f
jrafaaael <jrmd070102@gmail.com>
2023-04-27 17:12:51 -0400

    feat(mobile): add `isTitle` prop to `Text` component

--- apps/mobile/src/components/Text/Text.tsx
+++ apps/mobile/src/components/Text/Text.tsx
@@ -6,13 +6,24 @@ import { styles } from "./styles";
 
 interface BaseProps extends TextProps {
   size?: keyof typeof FONT_SIZE;
+  isTitle?: boolean;
 }
 
-export function Text({ children, size = "base", ...props }: BaseProps) {
+export function Text({
+  children,
+  size = "base",
+  isTitle,
+  ...props
+}: BaseProps) {
   return (
     <RNText
       {...props}
-      style={[styles.base, { fontSize: FONT_SIZE[size] }, props.style]}
+      style={[
+        styles.base,
+        { fontSize: FONT_SIZE[size] },
+        isTitle && styles.title,
+        props.style,
+      ]}
     >
       {children}
     </RNText>
--- apps/mobile/src/components/Text/styles.ts
+++ apps/mobile/src/components/Text/styles.ts
@@ -5,4 +5,7 @@ export const styles = StyleSheet.create({
   base: {
     color: COLORS.stone[50],
   },
+  title: {
+    fontWeight: "bold"
+  }
 });
