ccc85c3a7dd00a525e1b14146e2c3dd264d6255f
jrafaaael <jrmd070102@gmail.com>
2023-04-28 12:26:02 -0400

    feat(mobile): change text `send` to `Send` icon

--- apps/mobile/src/screens/Chat/components/MessageComposer/MessageComposer.tsx
+++ apps/mobile/src/screens/Chat/components/MessageComposer/MessageComposer.tsx
@@ -1,8 +1,9 @@
 import { useRef } from "react";
 import { Pressable, TextInput, View } from "react-native";
 
-import { Text } from "../../../../components/Text/Text";
+import { Send } from "../../../../components/icons/outlined/Send";
 import { COLORS } from "../../../../utils/styles";
+
 import { styles } from "./styles";
 
 interface Props {
@@ -22,8 +23,8 @@ export function MessageComposer({ onSubmit }: Props) {
           style={styles.input}
           onChangeText={(text) => (messageRef.current = text)}
         />
-        <Pressable onPress={() => onSubmit(messageRef.current)}>
-          <Text>Send</Text>
+        <Pressable style={styles.button} onPress={() => onSubmit(messageRef.current)}>
+          <Send width={20} height={20} stroke={COLORS.neutral[500]} />
         </Pressable>
       </View>
     </View>
