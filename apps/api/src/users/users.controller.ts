2c4ed57c8d5fad41fc34fdb921f246e61e6337b3
jrafaaael <jrmd070102@gmail.com>
2023-05-02 09:13:04 -0400

    chore(api): formatting

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -7,9 +7,8 @@ import {
   Post,
   Query,
 } from "@nestjs/common";
-import { CreateUserDto } from "./dto/create-user.dto";
-
 import { UserService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Controller("users")
 export class UsersController {
