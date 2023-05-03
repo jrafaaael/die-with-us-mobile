6762b19f38341c836b8d9ba03ab854d0a48d2665
jrafaaael <jrmd070102@gmail.com>
2023-05-03 11:36:45 -0400

    chore(mobile): remove trailing slash

--- apps/mobile/src/screens/auth/api/check-username-availability.ts
+++ apps/mobile/src/screens/auth/api/check-username-availability.ts
@@ -3,7 +3,7 @@ import { rq } from "../../../libs/axios";
 export async function checkUsernameAvailability(username: string): Promise<{
   available: boolean;
 }> {
-  const res = await rq.get(`users/username-available/`, {
+  const res = await rq.get(`users/username-available`, {
     params: { username },
   });
 
