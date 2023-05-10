2e648e77bacdacb60e998fa438b3b59d26af5373
jrafaaael <jrmd070102@gmail.com>
2023-05-10 17:09:11 -0400

    feat(mobile): inject `storedUsername` and `setStoredUsername` through context. create `useUser` hook

--- apps/mobile/src/providers/user-provider.tsx
+++ apps/mobile/src/providers/user-provider.tsx
@@ -1,18 +1,31 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { useMMKVString } from "react-native-mmkv";
 
 import { useProtectedRoute } from "../hooks/use-protected-route";
 
-const UserContext = createContext(null);
+interface ContextProps {
+  storedUsername: string;
+  setStoredUsername: (value: string | ((current: string) => string)) => void;
+}
+
+const UserContext = createContext<ContextProps>(null);
 
 interface Props {
   children: JSX.Element | JSX.Element[];
 }
 
 export function UserProvider({ children }: Props) {
-  const [username] = useMMKVString("username");
+  const [storedUsername, setStoredUsername] = useMMKVString("username");
 
-  useProtectedRoute(username);
+  useProtectedRoute(storedUsername);
 
-  return <UserContext.Provider value={{}}>{children}</UserContext.Provider>;
+  return (
+    <UserContext.Provider value={{ storedUsername, setStoredUsername }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useUser() {
+  return useContext(UserContext);
 }
