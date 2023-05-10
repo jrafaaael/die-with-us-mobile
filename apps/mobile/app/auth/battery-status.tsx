d9b66947448df76725ae531b72bf1c6730ebb2e5
jrafaaael <jrmd070102@gmail.com>
2023-05-10 13:00:42 -0400

    wip(mobile): try to redirect user when battery level is bigger than 5%

---
+++ apps/mobile/app/auth/battery-status.tsx
@@ -0,0 +1,11 @@
+import { Text } from "../../src/components/text";
+
+export function BatteryStatus() {
+  return (
+    <>
+      <Text isTitle size="xl">
+        Your battery is bigger than 5%
+      </Text>
+    </>
+  );
+}
--- apps/mobile/src/hooks/use-protected-route.ts
+++ apps/mobile/src/hooks/use-protected-route.ts
@@ -1,17 +1,21 @@
 import { useEffect } from "react";
 import { useRouter, useSegments } from "expo-router";
+import { useBattery } from "./use-battery";
 
 export function useProtectedRoute(username: string | null) {
   const segments = useSegments();
   const router = useRouter();
+  const { batteryLevel } = useBattery();
 
   useEffect(() => {
     const inAuthGroup = segments[0]?.includes("auth");
 
     if (!username && !inAuthGroup) {
       router.replace("/auth");
+    } else if (batteryLevel && batteryLevel > 5) {
+      router.replace("/auth/battery-status");
     } else if (username && inAuthGroup) {
       router.replace("/");
     }
-  }, [username, segments]);
+  }, [username, segments, batteryLevel]);
 }
