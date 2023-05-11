0e6e68f28e63f98782759d914494ec7668f7556b
jrafaaael <jrmd070102@gmail.com>
2023-05-11 18:12:26 -0400

    fix(mobile): remove unnecessary validation of segments

--- apps/mobile/src/hooks/use-protected-route.ts
+++ apps/mobile/src/hooks/use-protected-route.ts
@@ -1,27 +1,24 @@
 import { useEffect } from "react";
-import { useRouter, useSegments } from "expo-router";
+import { useRouter } from "expo-router";
 import { BatteryState } from "expo-battery";
 
 import { useBattery } from "./use-battery";
 import { MINIMUM_BATTERY_LEVEL } from "../utils/minimum-battery-level";
 
 export function useProtectedRoute(username: string | null) {
-  const segments = useSegments();
   const router = useRouter();
   const { batteryLevel, batteryState } = useBattery();
 
   useEffect(() => {
-    const inAuthGroup = segments[0]?.includes("auth");
-
-    if (!username && !inAuthGroup) {
+    if (!username) {
       router.replace("/auth");
     } else if (
       batteryLevel >= MINIMUM_BATTERY_LEVEL ||
       batteryState === BatteryState.CHARGING
     ) {
       router.replace("/auth/wrong-battery-status");
-    } else {
+    } else if (username) {
       router.replace("/");
     }
-  }, [username, segments, batteryLevel, batteryState]);
+  }, [username, batteryLevel, batteryState]);
 }
