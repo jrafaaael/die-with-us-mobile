a57e2aa822421cd1335d7d11d96a6cd3c6cac26e
jrafaaael <jrmd070102@gmail.com>
2023-05-28 13:06:18 -0400

    chore: remove unused code

--- src/screens/chat/components/message-list/styles.ts
+++ src/screens/chat/components/message-list/styles.ts
@@ -2,9 +2,6 @@ import { StyleSheet } from "react-native";
 import { SPACING } from "../../../../utils/styles";
 
 export const styles = StyleSheet.create({
-  list: {
-    flex: 1,
-  },
   separator: {
     height: SPACING.base,
   },
